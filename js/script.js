f9baebe4294c2a15c2873d4e7789812ac12265db
Farel Rizky <farelrizky69@gmail,com>
2024-08-02 17:12:18 +0700

    Update terbaru

--- js/script.js
+++ js/script.js
@@ -150,5 +150,24 @@ window.onclick = (e) => {
   }
 };
 
+// Form Validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
 // Feather icons initialization
 feather.replace();
